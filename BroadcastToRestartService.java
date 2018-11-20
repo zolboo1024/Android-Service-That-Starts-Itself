0f6a4cee9ef72cf6a2d45ed06e7786c76838a879
zolboo1024 <38169804+zolboo1024@users.noreply.github.com>
2018-11-19 23:40:40 -0500

    Update BroadcastToRestartService.java

--- BroadcastToRestartService.java
+++ BroadcastToRestartService.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 /**
  * Created by Zolboo Erdenebaatar
+ * Here's a simple BroadcastReceiver that 
+ * receives a signal when a service is killed or stopped without 
+ * your application knowing it. Then, it calls the same 
+ * service that just got killed to create a process that is not 
+ * to be stopped by Android Memory Manager. 
  */
 public class BroadcastToRestartService extends BroadcastReceiver {
     Intent newIntent;
