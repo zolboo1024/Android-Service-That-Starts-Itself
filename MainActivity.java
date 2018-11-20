bdec6720c5deda820f64d0773ffc4cde9b15fd5b
zolboo1024 <38169804+zolboo1024@users.noreply.github.com>
2018-11-19 23:42:13 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+/** 
+ * Create by Zolboo Erdenebaatar 08/31/2018
+ * Here's the entry point for the application. 
+ * On this activity, we have a single button thats
+ * only purpose is to start the service that is not 
+ * to be stopped. 
+ */
 public class MainActivity extends AppCompatActivity {
     Button startButton;
     @Override
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(getApplicationContext(), TypicalService.class);
+                //We start the service here.
                 startService(intent);
                 Log.d(this.getClass().getSimpleName(), "Service has started from the MainActivity");
             }
